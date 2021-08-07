da71dc877387c28920e8231b6f5aaa94d909cdd1
Nandini Agarwal <nia78831@gmail.com>
2021-08-07 09:48:20 +0530

    files

---
+++ client1/src/components/Chat.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Chat = () =>{
+    return(
+        <h1>CHAT</h1>
+    )
+}
+
+
+export default Chat;
---
+++ client1/src/components/Join.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Join = () =>{
+    return(
+        <h1>JOIN</h1>
+    )
+}
+
+
+export default Join;
